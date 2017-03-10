510769df8b40196c50e4b1e2c887527a7d7bc793
王凯 <vipwangkai@live.cn>
2017-03-10 18:46:35 +0800

    Add files via upload

--- GeekDjango/urls.py
+++ GeekDjango/urls.py
@@ -20,6 +20,6 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^hello/$', hello), 
+    url(r'^hello/$', views), 
     url(r'^hello/(\d+)/$', hello1), 
 ]
