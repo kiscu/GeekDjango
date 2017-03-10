e13a8e8c4ab9bde4704d31b53d42ebdd8727a277
王凯 <vipwangkai@live.cn>
2017-03-10 18:47:15 +0800

    Add files via upload

--- myLesson/views.py
+++ myLesson/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render,render_to_response
 from django.http import HttpResponse, Http404
 
 def hello(request):
@@ -10,3 +10,7 @@ def hello1(request, num):
 		num = int(num)
 	except ValueError:
 		raise Http404()
+
+
+def views(request):
+	return render_to_response('3.html', {})
