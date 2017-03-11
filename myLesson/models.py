ae7fc3c78487e97a40574a272ccf72026d4e722e
王凯 <vipwangkai@live.cn>
2017-03-11 14:46:11 +0800

    Add files via upload

--- myLesson/models.py
+++ myLesson/models.py
@@ -2,4 +2,15 @@ from __future__ import unicode_literals
 
 from django.db import models
 
-# Create your models here.
+class Mysite(models.Model):
+	title = models.CharField(max_length=100)
+	url = models.URLField()
+	author = models.CharField(max_length=100)
+	num = models.IntegerField()
+
+	def __unicode__(self):
+		return self.title
+
+	class Meta:
+		ordering = ['num']
+		
--- myLesson/views.py
+++ myLesson/views.py
@@ -1,8 +1,21 @@
+from myLesson.models import *
+from myLesson.forms import *
+
 from django.shortcuts import render,render_to_response
 from django.http import HttpResponse, Http404
 
 def hello(request):
-	return HttpResponse("hello world")
+	if request.method == 'POST':
+		form = Mybook(request.POST)
+		if form.is_valid():
+			data  = form.cleaned_data
+			title = data["title"]
+			return HttpResponse(title)
+
+	form = Mybook()
+	return render_to_response('4.html',{
+		'form':form
+		})
 
 
 def hello1(request, num):
