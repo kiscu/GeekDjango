e3976aec57fcf1fa935bb02906fa8c4e2ec30f82
王凯 <vipwangkai@live.cn>
2017-03-11 14:46:44 +0800

    Add files via upload

--- GeekDjango/settings.py
+++ GeekDjango/settings.py
@@ -44,7 +44,7 @@ MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
+    # 'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
@@ -76,8 +76,11 @@ WSGI_APPLICATION = 'GeekDjango.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'USER': 'root',
+        'PASSWORD': '$WangKai0712',
+        'NAME': 'GeekDjango',
+        'HOST': 'localhost'
     }
 }
 
